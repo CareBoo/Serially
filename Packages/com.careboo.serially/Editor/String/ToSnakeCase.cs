34b8228f0b56867a5ac5a9b260eaf960a21ebd73
Jason Bou Kheir <jason.boukheir@pm.me>
2020-07-07 20:47:52 -0700

    adding typepickerlistelement and typepickerwindow

---
+++ Packages/com.careboo.serially/Editor/String/ToSnakeCase.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+
+namespace CareBoo.Serially.Editor
+{
+    public static partial class StringExtensions
+    {
+        public static string ToSnakeCase(this string input)
+        {
+            var splitInput = input.Select(SeparateUpperCaseWithUnderscore);
+            string result = string.Concat(splitInput).ToLower();
+            return result;
+        }
+
+        private static string SeparateUpperCaseWithUnderscore(char element, int index)
+        {
+            var separator = index > 0 && char.IsUpper(element) ? "_" : "";
+            return $"{separator}{element}";
+        }
+    }
+}
