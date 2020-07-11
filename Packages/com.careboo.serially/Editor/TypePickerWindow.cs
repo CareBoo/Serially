7cdd6e142e80d25260b5c3e8ad99dc9faabeac9a
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2020-07-11 00:40:56 -0700

    finally got it workinggaa

--- Packages/com.careboo.serially/Editor/TypePickerWindow.cs
+++ Packages/com.careboo.serially/Editor/TypePickerWindow.cs
@@ -6,107 +6,114 @@ using static UnityEditor.EditorGUILayout;
 using static CareBoo.Serially.Editor.EditorGUIExtensions;
 using UnityEngine.UIElements;
 using UnityEditor.UIElements;
+using System.Collections.Generic;
 
 namespace CareBoo.Serially.Editor
 {
     public class TypePickerWindow : EditorWindow
     {
-        private const string TypeListName = "type-list";
-        private const string SearchFieldName = "search-field";
+        public const string TypeLabelName = "type-label";
+        public const string TypeListName = "type-list";
+        public const string SearchFieldName = "search-field";
+        public const string WindowAssetPath = "type_picker_window";
+        public const string ItemAssetPath = "type_picker_item";
 
-        private TypePickerListElement selected;
-        private Type preselectedType;
-        private Type[] types;
-        private Type[] searchedTypes;
+        private VisualTreeAsset itemVisualTreeAsset;
+
+        private Type selected;
+        private IEnumerable<Type> types;
+        private List<Type> searchedTypes;
         private Action<Type> onSelected;
         private string searchValue;
-
-        public Type SelectedType =>
-            selected?.Type ?? preselectedType;
+        private ListView listView;
 
         private ToolbarSearchField searchField;
-        private ToolbarSearchField SearchField =>
-            searchField
+
+        private ToolbarSearchField SearchField => searchField
             ?? (searchField = rootVisualElement.Q<ToolbarSearchField>(name: SearchFieldName));
 
-        private ListView listView;
-        public ListView ListView =>
-            listView
-            ?? (listView = rootVisualElement.Q<ListView>(name: TypeListName));
-
         public static TypePickerWindow ShowWindow(
-            Type preselectedType,
-            Type[] types,
+            Type selected,
+            IEnumerable<Type> types,
             Action<Type> onSelected,
             string title = null
             )
         {
             var window = (TypePickerWindow)CreateInstance(typeof(TypePickerWindow));
-            window.Init(preselectedType, types, onSelected, title);
+            window.InitData(selected, types, onSelected, title);
             window.ShowAuxWindow();
             return window;
         }
 
-        public void Init(Type preselectedType, Type[] types, Action<Type> onSelected, string title = null)
+        public void InitData(Type selected, IEnumerable<Type> types, Action<Type> onSelected, string title = null)
         {
-            this.preselectedType = preselectedType;
+            this.selected = selected;
             this.types = types;
             this.onSelected = onSelected;
             titleContent = new GUIContent(title ?? "Select Type");
             UpdateTypeSearch(null);
-            ResetListContent();
             SearchField.RegisterValueChangedCallback(UpdateTypeSearch);
         }
 
         private void OnEnable()
         {
-            AddWindowVisualElement();
+            InitWindow();
+            InitListView();
+            listView.onItemChosen += OnItemChosen;
         }
 
-        private void AddWindowVisualElement()
+        private void OnDisable()
         {
-            var assetPath = nameof(TypePickerWindow).ToSnakeCase();
-            var windowVisualTreeAsset = Resources.Load<VisualTreeAsset>(assetPath);
+            listView.onItemChosen -= OnItemChosen;
+        }
+
+        private void InitWindow()
+        {
+            var windowVisualTreeAsset = Resources.Load<VisualTreeAsset>(WindowAssetPath);
             windowVisualTreeAsset.CloneTree(rootVisualElement);
+            itemVisualTreeAsset = Resources.Load<VisualTreeAsset>(ItemAssetPath);
         }
 
-        private void ResetListContent()
+        private ListView InitListView()
         {
-            ListView.Clear();
-            AddTypeElement(null);
-            foreach (var type in searchedTypes)
-                AddTypeElement(type);
+            listView = rootVisualElement.Q<ListView>(name: TypeListName);
+            listView.selectionType = SelectionType.Single;
+            listView.itemsSource = searchedTypes;
+            listView.makeItem = MakeItem;
+            listView.bindItem = BindItem;
+            return listView;
         }
 
-        private void AddTypeElement(Type type)
+        private VisualElement MakeItem()
         {
-            var element = new TypePickerListElement(type, Select);
-            ListView.Add(element);
-            element.SetHighlight(type == SelectedType);
+            return itemVisualTreeAsset.CloneTree().GetFirstOfType<VisualElement>();
+        }
+
+        private void BindItem(VisualElement element, int index)
+        {
+            var type = searchedTypes.ElementAt(index);
+            var label = element.Q<Label>(name: TypeLabelName);
+            label.text = GetTypeLabelString(type);
+        }
+
+        private void OnItemChosen(object item)
+        {
+            onSelected?.Invoke((Type)item);
+            Close();
         }
 
         private void UpdateTypeSearch(ChangeEvent<string> stringChangeEvent)
         {
             searchValue = stringChangeEvent?.newValue;
-            searchedTypes = string.IsNullOrEmpty(searchValue)
-                ? types
-                : types.Where(IsInSearch).ToArray();
-            ResetListContent();
+            searchedTypes = types.Where(IsInSearch).Prepend(null).ToList();
+            listView.itemsSource = searchedTypes;
+            listView.selectedIndex = searchedTypes.IndexOf(selected);
         }
 
         private bool IsInSearch(Type type)
         {
-            return type.AssemblyQualifiedName.Contains(searchValue);
-        }
-
-        private void Select(TypePickerListElement element, MouseDownEvent evt)
-        {
-            selected?.SetHighlight(false);
-            selected = element;
-            selected?.SetHighlight(true);
-            onSelected?.Invoke(selected?.Type);
-            if (evt.clickCount >= 2)
-                Close();
+            return string.IsNullOrEmpty(searchValue)
+                || type.AssemblyQualifiedName.Contains(searchValue);
         }
     }
 }
