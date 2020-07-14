2b5d9e29737c837b07d268a345adfaed84f73115
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2020-07-14 01:34:23 -0700

    this won't work... I need it to be virtual to override it

---
+++ Packages/com.careboo.serially/Editor/UI/NotifyClassChange.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace CareBoo.Serially.Editor.UI
+{
+    public class NotifyClassChange : VisualElement
+    {
+        new public void AddToClassList(string className)
+        {
+            base.AddToClassList(className);
+        }
+
+        new public void RemoveFromClassList(string className)
+        {
+            base.RemoveFromClassList(className);
+        }
+    }
+}
