26fd74b40f72433eb48a783ba5ea4c54cacd8217
Jason Bou Kheir <jason.boukheir@pm.me>
2020-07-15 00:54:12 -0700

    fixing cyclomatic complexity and tests

---
+++ Packages/com.careboo.serially/Tests/Editor/ProvideSourceInfoAttributeTest.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Reflection;
+using NUnit.Framework;
+
+namespace CareBoo.Serially.Editor.Tests
+{
+    public class ProvideSourceInfoAttributeTest
+    {
+        [ProvideSourceInfo]
+        public class TestClass { }
+
+        [ProvideSourceInfo]
+        public string MyProperty { get; }
+
+        private ProvideSourceInfoAttribute GetAttribute(MemberInfo element)
+        {
+            var attr = Attribute.GetCustomAttribute(element, typeof(ProvideSourceInfoAttribute));
+            return (ProvideSourceInfoAttribute)attr;
+        }
+
+        private MemberInfo GetMemberInfo<TMember>(Expression<Func<ProvideSourceInfoAttributeTest, TMember>> expression)
+        {
+            var memberExpression = expression.Body as MemberExpression;
+            return memberExpression.Member;
+        }
+
+        [Test]
+        public void SettingAttributeShouldSetLineNumber()
+        {
+            var attribute = GetAttribute(typeof(TestClass));
+            Assert.AreEqual(10, attribute.LineNumber);
+        }
+
+        [Test]
+        public void SettingAttributeOnClassShouldSetMemberNameToEmpty()
+        {
+            var attribute = GetAttribute(typeof(TestClass));
+            Assert.AreEqual(string.Empty, attribute.Member);
+        }
+
+        [Test]
+        public void SettingAttributeOnPropertyShouldSetMemberNameToPropertyName()
+        {
+            var attribute = GetAttribute(GetMemberInfo(x => x.MyProperty));
+            Assert.AreEqual(nameof(MyProperty), attribute.Member);
+        }
+
+        [Test]
+        public void SettingAttributeShouldSetAssetPath()
+        {
+            var attribute = GetAttribute(typeof(TestClass));
+            var thisFileName = $"{nameof(ProvideSourceInfoAttributeTest)}.cs";
+            Assert.IsTrue(attribute.AssetPath.Contains(thisFileName));
+        }
+    }
+}
