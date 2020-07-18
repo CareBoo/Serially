2e3fd79e9cd31472ded3eb3a33e7003317a1b433
Jason Bou Kheir <jason.boukheir@pm.me>
2020-07-18 01:42:08 -0700

    almost 100% coverage in CareBoo.Serially

--- Packages/com.careboo.serially/Tests/Editor/SerializableTypeTest.cs
+++ Packages/com.careboo.serially/Tests/Editor/SerializableTypeTest.cs
@@ -1,11 +1,11 @@
 ﻿using System;
-using System.Collections;
-using System.IO;
 using System.Runtime.InteropServices;
 using NUnit.Framework;
 using UnityEditor;
+using UnityEditor.Build;
 using UnityEngine;
 using UnityEngine.TestTools;
+using static CareBoo.Serially.SerializableType;
 
 namespace CareBoo.Serially.Editor.Tests
 {
@@ -34,8 +34,8 @@ namespace CareBoo.Serially.Editor.Tests
         [TestCaseSource(nameof(TypeCases))]
         public void TryGetTypeShouldReturnSameTypeSerialized(Type expected)
         {
-            var typeString = SerializableType.ToSerializedType(expected);
-            var isSuccess = SerializableType.TryGetType(typeString, out var actual);
+            var typeString = ToSerializedType(expected);
+            var isSuccess = TryGetType(typeString, out var actual);
             Assert.IsTrue(isSuccess);
             Assert.AreEqual(expected, actual);
         }
@@ -43,13 +43,13 @@ namespace CareBoo.Serially.Editor.Tests
         [Test]
         public void ToSerializedTypeDoesNotFailWhenGivenNullType()
         {
-            var result = SerializableType.ToSerializedType(null);
+            var result = ToSerializedType(null);
             Assert.IsTrue(string.IsNullOrEmpty(result));
         }
 
         [Test]
         [TestCaseSource(nameof(TypeCases))]
-        public void SetTypeWithGuidAttributeShouldPersistAfterCallingSave(Type expected)
+        public void SetTypeShouldPersistAfterCallingSave(Type expected)
         {
             var asset = ScriptableObject.CreateInstance<SerializableTypeHolder>();
             asset.Instance = new SerializableType(expected);
@@ -57,6 +57,58 @@ namespace CareBoo.Serially.Editor.Tests
             asset = AssetDatabase.LoadAssetAtPath<SerializableTypeHolder>(AssetPath);
             var actual = asset.Instance.Type;
             Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void SetInvalidTypeIdShouldError()
+        {
+            var asset = ScriptableObject.CreateInstance<SerializableTypeHolder>();
+            asset.Instance = new SerializableType();
+            AssetDatabase.CreateAsset(asset, AssetPath);
+            var serializedObject = new SerializedObject(asset);
+            var typeIdProperty = serializedObject.FindProperty("Instance.typeId");
+            typeIdProperty.stringValue = "6366a62a-e654-4c53-9da5-d73fb875cd17";
+            serializedObject.ApplyModifiedProperties();
+            asset = AssetDatabase.LoadAssetAtPath<SerializableTypeHolder>(AssetPath);
+            var error = asset.Instance.TypeNotFoundError;
+            LogAssert.Expect(LogType.Error, error);
+        }
+
+        [Test]
+        [TestCaseSource(nameof(TypeCases))]
+        public void SettingTypeWhenBuildingSetsAssemblyQualifiedName(Type expected)
+        {
+            var asset = ScriptableObject.CreateInstance<SerializableTypeHolder>();
+            var serializableType = new SerializableType(expected);
+            asset.Instance = new SerializableType(expected);
+            (serializableType as IPreprocessBuildWithReport).OnPreprocessBuild(null);
+            AssetDatabase.CreateAsset(asset, AssetPath);
+            var serializedObject = new SerializedObject(asset);
+            var typeIdProperty = serializedObject.FindProperty("Instance.typeId");
+            var typeId = typeIdProperty.stringValue;
+            Assert.AreEqual(expected.AssemblyQualifiedName, typeId);
+            (serializableType as IPostprocessBuildWithReport).OnPostprocessBuild(null);
+        }
+
+        [Test]
+        public void IsBuildingIsTrueOnlyDuringBuild()
+        {
+            var serializableType = new SerializableType();
+            var actual = IsBuilding;
+            Assert.IsFalse(actual);
+            (serializableType as IPreprocessBuildWithReport).OnPreprocessBuild(null);
+            actual = IsBuilding;
+            Assert.IsTrue(actual);
+            (serializableType as IPostprocessBuildWithReport).OnPostprocessBuild(null);
+            actual = IsBuilding;
+            Assert.IsFalse(actual);
+        }
+
+        [Test]
+        public void CallbackOrderIs0()
+        {
+            var actual = new SerializableType();
+            Assert.AreEqual(0, actual.callbackOrder);
         }
     }
 }
