cc440b0df2e0d05bd86d413708e33fd4ab9a6bc8
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2020-07-20 06:41:40 -0700

    fixing bug with get array

--- Packages/com.careboo.serially/Editor/Extensions/SerializedProperty/GetValue.cs
+++ Packages/com.careboo.serially/Editor/Extensions/SerializedProperty/GetValue.cs
@@ -11,7 +11,7 @@ namespace CareBoo.Serially.Editor
         public static object GetValue(this SerializedProperty property,
             Func<IEnumerable<string>, IEnumerable<string>> pathModifier = null)
         {
-            IEnumerable<string> path = property.propertyPath.Replace(".Array.data[", "[").Split('.');
+            IEnumerable<string> path = property.propertyPath.Replace("Array.data[", "[").Split('.');
             if (pathModifier != null) path = pathModifier(path);
 
             var target = (object)property.serializedObject.targetObject;
@@ -25,8 +25,8 @@ namespace CareBoo.Serially.Editor
             var propertyStr = propertyPath.FirstOrDefault();
             if (propertyStr == null) return target;
 
-            target = propertyStr.TryGetArrayIndex(out propertyStr, out int index)
-                ? (target.GetFieldValue(propertyStr) as IEnumerable).ElementAtOrDefault(index)
+            target = propertyStr.TryGetArrayIndex(out int index)
+                ? (target as IEnumerable).ElementAtOrDefault(index)
                 : target.GetFieldValue(propertyStr);
 
             return target.GetValueRecur(propertyPath.Skip(1));
