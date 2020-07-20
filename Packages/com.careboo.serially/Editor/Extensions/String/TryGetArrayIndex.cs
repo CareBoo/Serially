e38e5ee7a0be392997260907fb772e35d98c4fe9
Jason Bou Kheir <jason.boukheir@pm.me>
2020-07-20 06:41:40 -0700

    fixing bug with get array

--- Packages/com.careboo.serially/Editor/Extensions/String/TryGetArrayIndex.cs
+++ Packages/com.careboo.serially/Editor/Extensions/String/TryGetArrayIndex.cs
@@ -4,21 +4,19 @@ namespace CareBoo.Serially.Editor
 {
     public static partial class StringExtensions
     {
-        private const string NameKey = "name";
 
         private const string IndexKey = "index";
 
         private static readonly Regex ArrayRegex = new Regex(
-            $"(?<{NameKey}>\\w+)\\[(?<{IndexKey}>\\d+)\\]",
+            $"\\[(?<{IndexKey}>\\d+)\\]",
             RegexOptions.Compiled);
 
-        public static bool TryGetArrayIndex(this string propertyStr, out string name, out int index)
+        public static bool TryGetArrayIndex(this string propertyStr, out int index)
         {
-            name = propertyStr; index = -1;
+            index = -1;
             var arrayMatch = ArrayRegex.Match(propertyStr);
             if (!arrayMatch.Success) return false;
 
-            name = arrayMatch.Groups[NameKey].Value;
             index = int.Parse(arrayMatch.Groups[IndexKey].Value);
             return true;
         }
