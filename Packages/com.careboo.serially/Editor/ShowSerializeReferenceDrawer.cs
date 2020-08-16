aafd1a10b69f2275cbeb8e77cb9b7ff977ce727b
Jason Bou Kheir <jason.boukheir@pm.me>
2020-08-16 02:25:10 -0700

    feat: :sparkles: add TypeFilterAttribute support
    Adding TypeFilterAttribute support to the ShowSerializeReference fields.
    
    Fixes #52

--- Packages/com.careboo.serially/Editor/ShowSerializeReferenceDrawer.cs
+++ Packages/com.careboo.serially/Editor/ShowSerializeReferenceDrawer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reflection;
 using UnityEditor;
 using UnityEngine;
 using static UnityEditor.EditorGUIUtility;
@@ -56,10 +57,10 @@ namespace CareBoo.Serially.Editor
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
             var currentEvent = GuiEvent.FromCurrentUnityEvent;
-            OnGUI(position, property, label, currentEvent);
+            OnGUI(position, property, label, currentEvent, fieldInfo);
         }
 
-        public static void OnGUI(Rect position, SerializedProperty property, GUIContent label, GuiEvent currentEvent)
+        public static void OnGUI(Rect position, SerializedProperty property, GUIContent label, GuiEvent currentEvent, FieldInfo fieldInfo)
         {
             if (currentEvent.Type == EventType.MouseDown
                 && position.Contains(currentEvent.MousePosition)
@@ -70,10 +71,11 @@ namespace CareBoo.Serially.Editor
                 currentEvent.Type = EventType.Used;
             }
 
+            var selectableTypes = new Lazy<IEnumerable<Type>>(() => GetSelectableTypes(property, fieldInfo));
             new TypeField(
                 position: LabelIndent(position),
                 selectedType: property.GetManagedReferenceValueType(),
-                selectableTypes: new Lazy<IEnumerable<Type>>(property.GetSelectableManagedReferenceValueTypes),
+                selectableTypes: selectableTypes,
                 onSelectType: property.SetManagedReferenceValueToNew,
                 currentEvent
                 ).DrawGui();
@@ -84,6 +86,14 @@ namespace CareBoo.Serially.Editor
         public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
         {
             return EditorGUI.GetPropertyHeight(property, label);
+        }
+
+        public static IEnumerable<Type> GetSelectableTypes(SerializedProperty property, FieldInfo fieldInfo)
+        {
+            var typeFilterAttribute = (TypeFilterAttribute)Attribute.GetCustomAttribute(fieldInfo, typeof(TypeFilterAttribute));
+            var parentObject = property.GetValue(p => p.SkipLast(1));
+            var filter = typeFilterAttribute.GetFilter(parentObject);
+            return filter(property.GetSelectableManagedReferenceValueTypes());
         }
     }
 }
