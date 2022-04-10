1ab4b02eaff122d281d1d95eb370841ac6d124cc
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2022-04-10 13:32:09 -0700

    fix(editor): fix getting field value on a property

--- Editor/Extensions/System.Object/GetFieldValue.cs
+++ Editor/Extensions/System.Object/GetFieldValue.cs
@@ -1,11 +1,26 @@
-﻿namespace CareBoo.Serially.Editor
+﻿using System;
+
+namespace CareBoo.Serially.Editor
 {
     public static partial class SystemObjectExtensions
     {
         public static object GetFieldValue(this object target, string fieldName)
         {
+            if (target == null)
+                throw new ArgumentNullException(nameof(target));
+
+            if (string.IsNullOrWhiteSpace(fieldName))
+                throw new ArgumentException("field cannot be null or whitespace", nameof(fieldName));
+
             var field = target.GetFieldInfo(fieldName);
-            return field.GetValue(target);
+            if (field != null)
+                return field.GetValue(target);
+
+            var property = target.GetPropertyInfo(fieldName);
+            if (property != null)
+                return property.GetValue(target);
+
+            return null;
         }
     }
 }
